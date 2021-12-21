7d668ca914360ec2f7e8b049b9a2d715722918b8
naoto-ysd <61533915+naoto-ysd@users.noreply.github.com>
2021-12-21 23:47:48 +0900

    Create newpage.vue

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::EmployeesController < ApiController
   before_action :set_employee, only: [:show]
 
+  # 拾えなかったExceptionが発生したら500 Internal server errorを応答する
+  rescue_from Exception, with: :render_status_500
+
   # ActiveRecordのレコードが見つからなければ404 not foundを応答する
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { error: '404 not found' }, status: 404
@@ -15,9 +18,30 @@ class Api::V1::EmployeesController < ApiController
     render json: @employee
   end
 
+  def create
+    employee = Employee.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_employee
     @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.fetch(:employee, {}).permit(:name, :department, :gender, :birth, :joined_date, :payment, :note)
+  end
+
+  def render_status_404(exception)
+    render json: { errors: [exception] }, status: 404
+  end
+
+  def render_status_500(exception)
+    render json: { errors: [exception] }, status: 500
   end
 end
