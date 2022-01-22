ff7557ac1bc3940fee92871cb79aa9877d780207
naoto-ysd <61533915+naoto-ysd@users.noreply.github.com>
2022-01-22 23:35:59 +0900

    Delete emloyees

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2021_12_11_054857) do
 
-  create_table "employees", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "department", default: "", null: false
-    t.integer "gender", default: 0, null: false
-    t.date "birth"
-    t.date "joined_date"
-    t.bigint "payment", default: 0, null: false
-    t.text "note", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "lock_version"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
