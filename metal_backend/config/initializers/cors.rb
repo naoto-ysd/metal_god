a79f751386432a410eb1ba1538bc5c04e8312cbe
naoto-ysd <61533915+naoto-ysd@users.noreply.github.com>
2022-03-11 23:53:30 +0900

    create api

--- metal_backend/config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV["API_DOMAIN"] || 'localhost:8080'
-
-    resource '*',
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
